14a704f4d70c1ded91ba79d4e15652a220158138
frequel <frequel45@gmail.com>
2024-06-28 08:30:09 +0545

    cors fix

--- src/app.js
+++ src/app.js
@@ -5,21 +5,24 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://project-manager-frontend-psi.vercel.app'
+   process.env.DEVELOPMENT_CORS_ORIGIN,
+   process.env.PRODUCTION_CORS_ORIGIN,
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true 
-}));
+app.use(
+   cors({
+      origin: function (origin, callback) {
+         if (!origin) return callback(null, true);
+         if (allowedOrigins.indexOf(origin) === -1) {
+            const msg =
+               "The CORS policy for this site does not allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+         }
+         return callback(null, true);
+      },
+      credentials: true,
+   })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
